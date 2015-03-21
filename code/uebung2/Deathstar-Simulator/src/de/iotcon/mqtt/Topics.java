deda9c7561f05712053568e383ef4c6dc9fb62d7
Christian Götz <christian.goetz@dc-square.de>
2015-03-21 17:54:29 +0100

    added code and slides

---
+++ code/uebung2/Deathstar-Simulator/src/de/iotcon/mqtt/Topics.java
@@ -0,0 +1,9 @@
+package de.iotcon.mqtt;
+
+/**
+ * @author Dominik Obermaier
+ */
+public class Topics {
+
+    public static final String REACTOR_ALERT = "deathstar/reactor/alert";
+}
---
+++ code/uebung3/Deathstar-Simulator/src/de/iotcon/mqtt/Topics.java
@@ -0,0 +1,11 @@
+package de.iotcon.mqtt;
+
+/**
+ * @author Dominik Obermaier
+ */
+public class Topics {
+
+    public static final String REACTOR_ALERT = "deathstar/reactor/alert";
+
+    public static final String SUPERLASER_STATUS = "deathstar/superlaser/status";
+}
